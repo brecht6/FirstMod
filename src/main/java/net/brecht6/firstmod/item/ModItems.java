0e55dc382304a15ee34a10e941d276736de57da9
Brecht987654321 <brechthosten14@gmail.com>
2022-03-06 15:34:15 +0100

    added banana

---
+++ src/main/java/net/brecht6/firstmod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.brecht6.firstmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.brecht6.firstmod.FirstMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item Banana = registerItem("banana",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(FirstMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+            FirstMod.LOGGER.info("Registering Mod Items for " + FirstMod.MOD_ID);
+        }
+}
