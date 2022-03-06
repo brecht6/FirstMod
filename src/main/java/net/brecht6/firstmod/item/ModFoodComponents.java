a5d21081f8b399f6a43f8fa093db04adc96952fa
Brecht987654321 <brechthosten14@gmail.com>
2022-03-06 18:27:18 +0100

    added lemon and you can eat the banana and lemon now

---
+++ src/main/java/net/brecht6/firstmod/item/ModFoodComponents.java
@@ -0,0 +1,7 @@
+package net.brecht6.firstmod.item;
+
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+    public static final FoodComponent fruit = new FoodComponent.Builder().hunger(4).saturationModifier(0.2f).build();
+}
--- src/main/java/net/brecht6/firstmod/item/ModItems.java
+++ src/main/java/net/brecht6/firstmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.brecht6.firstmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.brecht6.firstmod.FirstMod;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -9,7 +10,10 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item Banana = registerItem("banana",
-            new Item(new FabricItemSettings().group(ItemGroup.FOOD)));
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.fruit)));
+
+    public static final Item lemon = registerItem("lemon",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.fruit )));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(FirstMod.MOD_ID, name), item);
