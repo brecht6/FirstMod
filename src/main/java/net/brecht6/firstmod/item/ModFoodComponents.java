6a024283ac5b07ea68a8ed6ecdcb8ea9dfcd8814
Brecht987654321 <45824021+Brecht987654321@users.noreply.github.com>
2022-03-07 19:09:05 +0100

    Update ModFoodComponents.java

--- src/main/java/net/brecht6/firstmod/item/ModFoodComponents.java
+++ src/main/java/net/brecht6/firstmod/item/ModFoodComponents.java
@@ -3,5 +3,5 @@ package net.brecht6.firstmod.item;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent fruit = new FoodComponent.Builder().hunger(4).saturationModifier(0.2f).build();
+    public static final FoodComponent FRUIT = new FoodComponent.Builder().hunger(4).saturationModifier(0.2f).build();
 }
